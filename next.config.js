083355e6212eb4fe5ccb42a9399dbecef4f98e7b
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2021-11-20 15:27:58 +0100

    update

--- next.config.js
+++ next.config.js
@@ -4,17 +4,9 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       reactStrictMode: true,
-      env: {
-        user: "stacktechagency@gmail.com",
-        password: "ybtapqblkpwcoous",
-      },
     };
   }
   return {
     reactStrictMode: true,
-    env: {
-      user: "stacktechagency@gmail.com",
-      password: "ybtapqblkpwcoous",
-    },
   };
 };
