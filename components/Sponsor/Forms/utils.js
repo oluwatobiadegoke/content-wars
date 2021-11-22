41fb15ae111fd36e0059935e70045e0cf2606dd4
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2021-11-22 12:26:57 +0100

    sponsor form and api email update

--- components/Sponsor/Forms/utils.js
+++ components/Sponsor/Forms/utils.js
@@ -5,14 +5,14 @@ export const validationSchema = Yup.object().shape({
   Lastname: Yup.string().required("Lastname is required"),
   Email: Yup.string().required("Email is required"),
   Phone: Yup.string().required("Phone is required"),
-  Industry: Yup.string().required("Industry is required"),
-  Socialmedia: Yup.string().required("Socialmedia is required"),
-  Followers: Yup.string().required("Followers is required"),
-  Twitch: Yup.string().required("Twitch is required"),
-  Reddit: Yup.string().required("Reddit is required"),
-  Youtube: Yup.string().required("Youtube is required"),
-  Tiktok: Yup.string().required("Tiktok is required"),
-  Instagram: Yup.string().required("Instagram is required"),
+  Industry: Yup.string(),
+  Socialmedia: Yup.string(),
+  Followers: Yup.string(),
+  Twitch: Yup.string(),
+  Reddit: Yup.string(),
+  Youtube: Yup.string(),
+  Tiktok: Yup.string(),
+  Instagram: Yup.string(),
 });
 
 export const initialValues = {
--- pages/api/sponsor.js
+++ pages/api/sponsor.js
@@ -36,37 +36,85 @@ export default async function handler(req, res) {
         <p><strong>Name: </strong> ${Firstname + " " + Lastname}</p>
         <p><strong>Email: </strong> ${Email}</p>
         <p><strong>Phone number: </strong>${Phone}</p>
-        <p><strong>Industry: </strong> ${Industry}</p>
-        <p><strong>Social media: </strong> ${Socialmedia}</p>
-        <p><strong>Followers: </strong>${Followers}</p>
-        <p><strong>Twitch: </strong> ${Twitch}</p>
-        <p><strong>Reddit: </strong> ${Reddit}</p>
-        <p><strong>Youtube: </strong>${Youtube}</p>
-        <p><strong>Tiktok: </strong>${Tiktok}</p>
-        <p><strong>Instagram: </strong>${Instagram}</p>
+        ${
+          Industry && (
+            <p>
+              <strong>Industry: </strong> ${Industry}
+            </p>
+          )
+        }
+        ${
+          Socialmedia && (
+            <p>
+              <strong>Social media: </strong> ${Socialmedia}
+            </p>
+          )
+        }
+        ${
+          Followers && (
+            <p>
+              <strong>Followers: </strong>${Followers}
+            </p>
+          )
+        }
+        ${
+          Twitch && (
+            <p>
+              <strong>Twitch: </strong> ${Twitch}
+            </p>
+          )
+        }
+        ${
+          Reddit && (
+            <p>
+              <strong>Reddit: </strong> ${Reddit}
+            </p>
+          )
+        }
+        ${
+          Youtube && (
+            <p>
+              <strong>Youtube: </strong>${Youtube}
+            </p>
+          )
+        }
+        ${
+          Tiktok && (
+            <p>
+              <strong>Tiktok: </strong>${Tiktok}
+            </p>
+          )
+        }
+        ${
+          Instagram && (
+            <p>
+              <strong>Instagram: </strong>${Instagram}
+            </p>
+          )
+        }
 
       `,
     });
     const themail = await transporter.sendMail({
       from: "natalie@contentwars.io",
       to: Email,
-      subject: `Almost done with your application is almost complete :)`,
+      subject: `Your application is almost complete :)`,
       html: `
         <p>Hi ${Firstname},</p>
 
-        <p>We’re almost at the finish line! Thank you for your consideration in applying to be sponsored by choosing to submit a Content Wars sponsorship application!</p>
+        <p>We’re almost at the finish line! Thank you for choosing to submit a Content Wars sponsorship application!</p>
 
-        <p>Imagine being if you had been involved with Tik Tok or Instagram right from the get-go when they first startedlaunched and thathen imagine those companies paid you in crypto and for every like/view your content received—in turn making you money. got you made money? Well, that’s just what this is!</p>
+        <p>Imagine if you had been involved with TikTok or Instagram right from the get-go when they launched and that those companies paid you in crypto and for every like/view your content received—in turn making you money. Well, that’s just what this is!</p>
 
-        <p>We’re excited to welcomebring talented influencers like yourself into ourthe Content Wars  family. Content Wars Our platform is here to help influencers expand and create new revenue streams, and  also servinge as a diversification strategy (or a “plan B”) in the event that sSocial mMedia giants throttle our hard-earned revenue—… which is honestly not so hard to believe in this day and age!</p>
+        <p>We’re excited to welcome talented influencers like yourself into our Content Wars family. Our platform is here to help influencers expand and create new revenue streams, and also serving as a diversification strategy (or a “plan B”) in the event that social media giants throttle our hard-earned revenue—which is honestly not so hard to believe in this day and age!</p>
 
-        <p>Once After we finish reviewing your application, we’ll let you know if you’re approved , and, if so, will be sending you another email with a code so you can download the bBeta aApp upon itsonce released., You can also set up your cCrypto pPayment method by applying to be sponsored bysubmitting a Content Coin sponsorship application—the official crypto coin of Ccontent Wwars.</p>
+        <p>After we finish reviewing your application, we’ll let you know if you’re approved and, if so, send you an email with a code so you can download the beta app upon its release. You can also set up your crypto payment method by submitting a Content Coin sponsorship application—the official crypto coin of Ccontent Wars.</p>
 
         <p>https://contentecoin.com/sponsor</p>
 
-        <p>You can use the same code once As soon as you’re approved,  to have your followers can use the same code to download the app and when they buy Content Coin you will getreceive a 4% discount forof every Content Coin purchase they make., Yes, you heard that right that’s real!  Users will be provided coins Wwhen they download the app, users will receive coins that you get to keep if they “like” your content and watch ads or purchase from our advertisers.and when they watch Ads or purchase from our advertisers with that then they can like your content and you get to keep those coins! </p>
+        <p>As soon as you’re approved, your followers can use the same code to download the app and receive a 4% discount for every Content Coin purchase they make. Yes, you heard that right! When they download the app, users will receive coins that you get to keep if they “like” your content and watch ads or purchase from our advertisers. </p>
 
-        <p>All you have to do nowthat’s left for you to do is to anxiously await to hear back from usour response, once you do you can get startedafter which you can create your account and can start sharing your code with your followers.</p>
+        <p>All you have to do is to anxiously await our response, after which you can create your account and share your code with your followers.</p>
 
 
         <p>Chat soon!</p>
