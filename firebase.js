9b240657d2da73a59b9727972b6494b05a75961f
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2021-12-21 11:21:23 +0100

    adding firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAPOwRwFSYyfnNZllZtvlEem8lcc3GYcQ",
+  authDomain: "contentwars-e965a.firebaseapp.com",
+  projectId: "contentwars-e965a",
+  storageBucket: "contentwars-e965a.appspot.com",
+  messagingSenderId: "463214878662",
+  appId: "1:463214878662:web:f49a9e42caee9a1a7edcc7",
+  measurementId: "G-G95GD0CD5B",
+};
